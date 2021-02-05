ce9a0b81f1723f256c22811e86bba567cedc5c73
DilipKheni10001 <discussionforall@gmail.com>
2021-02-05 18:09:23 +0530

    update before_install.sh file

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,17 +1,34 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Install node.js
-sudo apt-get install python-software-properties -y
-sudo apt-add-repository ppa:chris-lea/node.js -y
-sudo apt-get update
-sudo apt-get install nodejs -y
+# # Install node.js
+# sudo apt-get install python-software-properties -y
+# sudo apt-add-repository ppa:chris-lea/node.js -y
+# sudo apt-get update
+# sudo apt-get install nodejs -y
 
-# Install nodemon
-# sudo npm install nodemon -g
+# # Install nodemon
+# # sudo npm install nodemon -g
 
-# Install forever module 
-# https://www.npmjs.com/package/forever
-sudo npm install forever -g
+# # Install forever module 
+# # https://www.npmjs.com/package/forever
+# sudo npm install forever -g
 
-# Clean working folder
-# sudo find /home/ec2-user/test -type f -delete
+# # Clean working folder
+# # sudo find /home/ec2-user/test -type f -delete
+
+#!/usr/bin/env bash
+set -e
+
+# update instance
+yum -y update
+
+# install general libraries like Java or ImageMagick
+yum -y install default-jre ImageMagick
+
+# add nodejs to yum
+curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
+yum -y install nodejs #default-jre ImageMagick
+
+# install pm2 module globaly
+npm install -g pm2
+pm2 update
